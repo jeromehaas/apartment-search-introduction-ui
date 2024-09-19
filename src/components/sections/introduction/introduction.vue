a09b3d5f51210945412fc0947868c471643558d3
jeromehaas <hello@jeromehaas.ch>
2024-09-19 11:43:00 +0200

    Setup: section components

--- src/components/sections/introduction/introduction.vue
+++ src/components/sections/introduction/introduction.vue
@@ -1,3 +1,15 @@
+<script setup>
+
+// IMPORTS
+import './introduction.scss';
+import Profile from '@/components/blocks/profile/profile.vue';
+import Metadata from '@/components/blocks/metadata/metadata.vue';
+
+</script>
+
 <template>
- Introduction
+  <div class="introduction">
+    <Metadata class="introduction__metadata"/>
+    <Profile class="introduction__profile" />
+  </div>
 </template>
